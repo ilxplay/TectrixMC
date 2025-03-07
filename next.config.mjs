ad393ff1d54d4fa54bac8147aae2f40c52c53034
ilxplay <illiasuper46@gmail.com>
2025-03-08 00:12:53 +0100

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 export default (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
@@ -7,6 +8,10 @@ export default (phase) => {
   const nextConfig = {
     experimental: {
       forceSwcTransforms: true,
+    },
+    output: "export",
+    images: {
+      unoptimized: true,
     },
   };
   return nextConfig;
