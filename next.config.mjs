3042c9c1c5f3899694cb367c7a3dc21a0b1d8e8d
TecTrix <joranhennion@duck.com>
2025-03-07 19:30:46 +0100

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,2 @@
-// @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
-export default (phase) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-  };
-  return nextConfig;
-};
+const nextConfig = {};
+return nextConfig;
