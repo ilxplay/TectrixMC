6c85ac2270ec47a5ae58290cede0d3da4ef014b1
TecTrix <joranhennion@duck.com>
2025-03-07 19:32:16 +0100

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,11 @@
-const nextConfig = {};
-return nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
