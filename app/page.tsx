16fa9d89460228630a9e3243a2366788cde0f1b8
TecTrix <joranhennion@duck.com>
2025-03-07 20:22:31 +0100

    try and remove client rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { navItems } from "@/data";
-import Head from "next/head";
-import dynamic from "next/dynamic";
+import { Helmet } from "react-helmet";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
@@ -14,29 +11,17 @@ import Approach from "@/components/Approach";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Features from "@/components/Features";
 
-// Dynamically import components that might use `document`
-const DynamicFloatingNav = dynamic(
-  () => import("@/components/ui/FloatingNavbar"),
-  { ssr: false }
-);
-
 const Home = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <Head>
+      <Helmet>
         <link
           rel="icon"
           href="faviconTectrix.ico"
         />
-      </Head>
+      </Helmet>
       <div className="max-w-7xl w-full">
-        {isClient && <DynamicFloatingNav navItems={navItems} />}
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         {/*<Players />*/}
