fddea4c5aede54d738cab3aadaf686677627b447
TecTrix <joranhennion@duck.com>
2025-03-07 20:17:34 +0100

    fix try #69

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { navItems } from "@/data";
-import { Helmet } from "react-helmet";
+import Head from "next/head";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
@@ -16,12 +16,12 @@ import Features from "@/components/Features";
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <Helmet>
+      <Head>
         <link
           rel="icon"
           href="faviconTectrix.ico"
         />
-      </Helmet>
+      </Head>
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
