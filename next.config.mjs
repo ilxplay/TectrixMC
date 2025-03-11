f9197acc39b7802a4856b28256b80a1dcbf1a7b6
ilxplay <121191918+ilxplay@users.noreply.github.com>
2025-03-11 11:31:05 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
-export default (phase) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    experimental: {
-      forceSwcTransforms: true,
-    },
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-  };
-  return nextConfig;
-};
+ import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+ export default (phase) => {
+   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+ @@ -9,12 +8,6 @@ export default (phase) => {
+     experimental: {
+       forceSwcTransforms: true,
+     },
+     output: "export",
+     basePath: isDev ? undefined : "/TectrixMC",
+     assetPrefix: isDev ? undefined : "/TectrixMC",
+     images: {
+       unoptimized: true,
+     },
+   };
+   return nextConfig;
+ };
